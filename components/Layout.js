17009178abea1dcfcc41f6231f8984ec52fb130d
Lumsden Ruaidhri (BICS) <LumsdenR@sussex.nhs.uk>
2021-04-04 22:13:28 +0100

    sticky footer

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 
+import styles from '../styles/Layout.module.css'
+
 
 const Layout = (props) => {
 
@@ -11,9 +13,11 @@ const Layout = (props) => {
     // }
 
     return (
-        <div>
-            <Header />
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <Header />
                 {props.children}
+            </div>
             <Footer />
         </div>
     )
