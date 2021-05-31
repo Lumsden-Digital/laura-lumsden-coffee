2f2d73ff68db5c537e2d0b3e2f0be1d807b14507
Lumsden Ruaidhri (BICS) <LumsdenR@sussex.nhs.uk>
2021-05-31 07:27:40 +0100

    generated one static image as example

--- pages/blog/[post].js
+++ pages/blog/[post].js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Container } from 'react-bootstrap'
 import Link from 'next/link'
 import Image from 'next/image'
+import imageUrlBuilder from '@sanity/image-url'
 
 // *[_type == 'post' && title == 'The Recessionists' ] | order(publishedAt desc) {
 //     body
 //   } [0]
 
-export const getServerSideProps = async pageContext => {
+export const getServerSideProps = async pageContext => {   
+
     const pageSlug = pageContext.query.post
 
     if(!pageSlug) {
@@ -38,10 +40,23 @@ export const getServerSideProps = async pageContext => {
 }
 
 const Post = ({ title, body, url }) => {
+
+    const [imageUrl, setImageUrl] = useState('')
+
+    const foo = 'image-dc1550489483e3c151852f4f59bba7a5802fc09b-605x907-jpg'
+
+    useEffect(() => {
+        const imageBuilder = imageUrlBuilder({
+            projectId: '5n04ir7t',
+            dataset: 'production'
+        })
+
+        setImageUrl(imageBuilder.image(foo))
+    },[foo])
     // console.log(body[0].children[0].text)
     // console.log(body.map(part => part.children[0].text))
-    console.log(body)
-    console.log(url)
+    console.log(body[9].asset._ref)
+
     return (
         <Container className='p-5'>
             <Link href='/blog' passHref><h6 className='btn'>back</h6></Link>            
@@ -59,7 +74,13 @@ const Post = ({ title, body, url }) => {
                     )
                 }
 
-                return <p>{part.children[0].text}</p>
+                return (
+                    <div>
+                        {imageUrl && <img src={imageUrl}/>}
+                        <p>{part.children[0].text}</p>                        
+                    </div>
+                )
+
             })}
             <Link href='/blog' passHref><h6 className='btn mt-4'>back</h6></Link>            
         </Container> 
