3c2654df0bc17230781f8b3f5f38c5a0ca9edbff
Ruaidhri Lumsden <roarbot@pop-os.localdomain>
2021-08-23 22:27:31 +0100

    rendered book images

--- pages/shop.js
+++ pages/shop.js
@@ -1,14 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 import { Container } from 'react-bootstrap'
 import { booksTest}  from '../components/test'
 import Link from 'next/link'
+import imageUrlBuilder from '@sanity/image-url'
 
 const Shop = ({ books }) => {
+
+    const [builtImageArray, setBuiltImageArray] = useState([])
+
+    const imageBuilder = imageUrlBuilder({
+        projectId: '5n04ir7t',
+        dataset: 'production'
+    })
 
     const authorPage = 'https://www.amazon.co.uk/N-W-Twyford/e/B07HZ11ZWC?ref=sr_ntt_srch_lnk_3&qid=1621795620&sr=8-3'
 
     useEffect(() => {
+        const imageArray = books[0].map(b => imageBuilder.image(b.image.asset._ref))
+        setBuiltImageArray(imageArray)
     },[])
 
     // console.log(books[0])
@@ -24,14 +34,15 @@ const Shop = ({ books }) => {
             <Container>  
                 <h3 className="section-header my-4">Shop</h3>
                 <p>Check out my <a href={authorPage}>Amazon author page</a> for my published work.</p>   
-                {books[0].map(book => (
+                {books[0].map((book, i) => (
                     <div className='box' key={`${book.title}`}> 
-                        <Link href={`/blog/${book.slug.current}`} passHref>
+                        <Link href={`/blog`} passHref>
                             <div className='box-content'>
                                 <div>
                                     <h5>{book.title}</h5>
                                 </div>
-                                <span>sdfsd</span>
+                                {/* <span>{book.description[0].children[0].text}</span> */}
+                                <img src={builtImageArray[i]} style={{margin: '2.5rem'}}/>
                             </div>
                         </Link>
                     </div> 
